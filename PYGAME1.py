e7674a4380d5b55d21d1133bb21ccd96745fa660
thebatcn <62248602+thebatcn@users.noreply.github.com>
2020-03-17 22:55:47 +0800

    Add files via upload

---
+++ PYGAME1.py
@@ -0,0 +1,20 @@
+import pygame,sys,random
+import math
+from pygame.color import THECOLORS
+
+pygame.init()
+screen = pygame.display.set_mode([640,480])
+screen.fill([255,255,255])
+plotPoints= []
+for x in range(0,640):
+    y= int(math.sin(x/640.0*4*math.pi)*200+240)
+    plotPoints.append([x,y])
+    pygame.draw.lines(screen,[0,0,0],False,plotPoints,1)
+    pygame.display.flip()
+    
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
