017b81170f5be67a877ecbca5a871c2e155d8d50
unknown <liuzuyang@piccad.intra>
2020-03-18 13:15:35 +0800

    for x,y in (a,b)练习遍历元组的循环

---
+++ items.py
@@ -0,0 +1,11 @@
+
+#!/usr/bin/python
+# coding=utf-8
+ 
+dict = {'Google': 'www.google.com', 'Runoob': 'www.runoob.com', 'taobao': 'www.taobao.com'}
+ 
+print ("字典值 :{}".format(dict.items()))
+ 
+# 遍历字典列表
+for key,values in  dict.items():
+    print (key,values)
