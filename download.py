71066f904dff78018dc85df2c2dae14cb79cbb63
thebatcn <thebat@163.com>
2022-09-04 01:19:12 +0800

    web练习，编写download函数

---
+++ download.py
@@ -0,0 +1,24 @@
+import urllib.request
+from urllib.error import URLError,HTTPError,ContentTooShortError
+
+def download(url,user_agent ='wswp',num_retries=2):
+    print("Downloading {}".format(url))
+    request = urllib.request.Request(url)
+    request.add_header("User-agent",user_agent)
+
+    try:
+       html =  urllib.request.urlopen(url).read()
+    except (URLError,HTTPError,ConnectionAbortedError)as e:
+        print("Download error: ",e.reason)
+        html = None
+        if num_retries > 0:
+            if hasattr(e,'code') and 500<=e.code<=600:
+            #recursively retry 5xx HTTP errors
+                download(url,'wswp',num_retries-1) 
+    return html
+
+    
+url = 'https://www.baidu.com'
+
+download(url)
+
