e5c1508476cbb281d1c8fa39fbe014229cb535b1
thebatcn <thebat@163.com>
2022-09-04 14:31:52 +0800

    练习

--- turtle练习.py
+++ turtle练习.py
@@ -7,4 +7,15 @@ for i in range(0,360,30):
     turtle.forward(50)
     turtle.right(30)
 
+turtle.goto(255,255)
+turtle.pencolor('blue')
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
 turtle.mainloop()
