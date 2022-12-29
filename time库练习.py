58e3cf762290719a3ec142aa16776fa9fb485683
thebatcn <62248602+thebatcn@users.noreply.github.com>
2022-12-29 21:51:44 +0800

    Update time库练习.py

--- time库练习.py
+++ time库练习.py
@@ -1,9 +1,12 @@
 import time
+#time.gmtime()、time.localtime() 获取当前时间 的tuple或者struct_time  结果
+#time.strftime(format,tuple)，按照格式转换显示。要时间tuple 或者 struct_time .
 
 a = 1000
 
-print('asctime ',time.asctime(time.gmtime()))
-print('asctime ',time.asctime())
+print('asctime ',time.asctime(time.gmtime()))       # 获取当前时间字符串
+print('asctime ',time.asctime())                    # 获取当前时间字符串
+print('ctime()',time.ctime())                       # 获取当前时间字符串
 print('ctime ',time.ctime(a))
 print('gmtime ',time.gmtime(a))
 print('localtime ',time.localtime(a))
