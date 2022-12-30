f51bd8fd1e01f6db85994633b980319e0c5db5ec
thebatcn <thebatcn1@hotmail.com>
2022-12-30 17:33:40 +0800

    ex6

--- Learn PYTHON3 the HARD WAY/ex6.py
+++ Learn PYTHON3 the HARD WAY/ex6.py
@@ -1,3 +1,22 @@
-type_of_people = 10
-x =f"There are {type_of_people} types of people."
+type_of_people = 10              #设置变量
+x =f"There are {type_of_people} types of people."  #x 赋值。 
 
+binary = "binary"
+do_not = "don't"
+y = f"Those who know {binary} and those who {do_not}"
+
+print(x)
+print(y)
+
+print(f"I said: {x}")
+print(f"I also said: '{y}'")
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! {}"
+
+print(joke_evaluation.format(hilarious))   #另一种方式{}字符串赋值
+
+w = "This is the left side of ..."
+e ="a string with a right side."
+
+print(w + e)
