fd20f094438cbfe0893b2db37773ec885b7d4ed2
thebatcn <thebatcn1@hotmail.com>
2023-01-01 23:39:44 +0800

    修改 ex13

---
+++ Learn PYTHON3 the HARD WAY/ex13.py
@@ -0,0 +1,12 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+
+in1 = input("This is from input: ")
+print(in1)
+print('type(first): ',type(first))
