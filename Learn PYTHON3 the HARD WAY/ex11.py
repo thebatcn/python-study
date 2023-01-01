636e030116f0bb85540f1c20b6ac4af53f831d96
thebatcn <thebatcn1@hotmail.com>
2023-01-01 23:10:25 +0800

    ex12改下

--- Learn PYTHON3 the HARD WAY/ex11.py
+++ Learn PYTHON3 the HARD WAY/ex11.py
@@ -1,8 +1,12 @@
-print("How old are you?", end=' ')
-age = input()
-print("How tall are you?", end=' ')
-height = input()
-print("How much do you weight?", end=' ')
-weight = input()
+# print("How old are you?", end=' ')
+# age = input()
+# print("How tall are you?", end=' ')
+# height = input()
+# print("How much do you weight?", end=' ')
+# weight = input()
+
+age = input("How old are you?")
+height = input("How tall are you?")
+weight = input("How much do you weight?")
 
 print(f"So, you're {age} old, {height} tall and {weight} heavy.")
