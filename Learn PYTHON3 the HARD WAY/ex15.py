446ef4c77c4a933c2a264505571e12ba44bdfa29
thebatcn <thebatcn1@hotmail.com>
2023-01-02 19:31:22 +0800

    ex15.py修改

--- Learn PYTHON3 the HARD WAY/ex15.py
+++ Learn PYTHON3 the HARD WAY/ex15.py
@@ -7,3 +7,11 @@ txt = open(filename)
 print(f"Here's your file {filename}:")
 
 print(txt.read())
+# txt.close()
+print("Type the filename again: ")
+file_again = input("> ")
+
+txt_again = open(file_again)
+
+print(txt_again.read())
+# txt_again.close()
