ebd09045ada70f68793556e1732b25bc44c8ba31
thebatcn <thebatcn1@hotmail.com>
2023-01-07 23:02:41 +0800

    修改

---
+++ Learn PYTHON3 the HARD WAY/ex33.py
@@ -0,0 +1,30 @@
+i = 0
+numbers = []
+
+while i < 6:
+    print(f"At the top i is {i}")
+    numbers.append(i)
+
+    i = i + 1
+    print("Numbers now: ",numbers)
+    print(f"At the bottom i is {i}")
+
+
+print("The numbers: ")
+
+for num in numbers:
+    print(num)
+
+
+def loop(j):
+
+    i = 0
+    while i < j:
+        print(f"At the top i is {i}")
+        numbers.append(i)
+
+        i = i + 1
+        print("Numbers now: ",numbers)
+        print(f"At the bottom i is {i}")
+
+loop(10)
