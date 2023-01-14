de358cfb009ea0393ec8ee5a8791e3bce2ad01bc
thebatcn <thebatcn1@hotmail.com>
2023-01-14 22:11:52 +0800

    set 方法

---
+++ Set 方法.py
@@ -0,0 +1,4 @@
+# 返回差集   set.symmetric_difference   set.symmetric_difference.update 
+# 返回交集   set.intersection(), set.intersection.update()
+# 并集      set.union
+# (本集有其他集没有的）all elements that are in this set but not the others.    set.difference(), set.difference.update()
