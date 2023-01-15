082a43668d35b5b67a36b898b8adadf027dcec1e
thebatcn <thebatcn1@hotmail.com>
2023-01-15 12:23:47 +0800

    ex40相关文件

---
+++ Learn PYTHON3 the HARD WAY/ex40.py
@@ -0,0 +1,9 @@
+"""mystuff module a test """
+
+mystuff = {"apple":"I AM APPLES!"}
+print(mystuff['apple'])
+
+
+import mystuff
+
+mystuff.apple()
---
+++ Learn PYTHON3 the HARD WAY/mystuff.py
@@ -0,0 +1,4 @@
+# this goes in mystuff.py
+
+def apple():
+    print("I AM APPLES!")
