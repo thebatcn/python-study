2648169291446289b2e9952659eda36ea76528a5
thebatcn <thebatcn1@hotmail.com>
2023-01-15 16:56:55 +0800

    修改

--- Learn PYTHON3 the HARD WAY/ex40.py
+++ Learn PYTHON3 the HARD WAY/ex40.py
@@ -14,3 +14,22 @@ print(mystuff.tangerine)
 thing = mystuff.Mystuff()
 thing.apple()
 print(thing.tangering)
+
+class Song():
+
+    def __init__(self, lyrics) -> None:
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line)
+
+happy_bday = Song(["Happy birthday to you",
+                    "I don't want to get sued",
+                    "So I'll stop right the re"])
+
+bulls_on_parade = Song(["They rally around the family",
+                        "With pockets full of shells"])
+
+happy_bday.sing_me_a_song()
+bulls_on_parade.sing_me_a_song()
