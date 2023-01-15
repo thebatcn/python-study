5c2e45479d21cfdda5193857b54a8df6de396d26
thebatcn <thebatcn1@hotmail.com>
2023-01-15 12:51:17 +0800

    3种方法可以从某个东西里获取东西：

--- Learn PYTHON3 the HARD WAY/ex40.py
+++ Learn PYTHON3 the HARD WAY/ex40.py
@@ -1,9 +1,16 @@
-"""mystuff module a test """
 
+# dict style
 mystuff = {"apple":"I AM APPLES!"}
 print(mystuff['apple'])
 
 
 import mystuff
 
+# moudle style
 mystuff.apple()
+print(mystuff.tangerine)
+
+# class style
+thing = mystuff.Mystuff()
+thing.apple()
+print(thing.tangering)
--- Learn PYTHON3 the HARD WAY/mystuff.py
+++ Learn PYTHON3 the HARD WAY/mystuff.py
@@ -1,4 +1,16 @@
 # this goes in mystuff.py
+"""mystuff module a test """
 
 def apple():
     print("I AM APPLES!")
+
+# this is just a variable
+tangerine = "Living rlflection of a dream"
+
+class Mystuff(object):
+
+    def __init__(self):
+        self.tangering = "And now a thousand years between"
+
+    def apple(self):
+        print("I AM CLASS APPLES!")
