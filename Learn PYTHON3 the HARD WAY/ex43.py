a29190734b766990c5d3156153febee29c88dfa0
thebatcn <thebatcn1@hotmail.com>
2023-01-23 19:36:58 +0800

    ex43录入书上代码

--- Learn PYTHON3 the HARD WAY/ex43.py
+++ Learn PYTHON3 the HARD WAY/ex43.py
@@ -1,21 +1,80 @@
-""" from ex43_classes import *
+from sys import exit
+from random import randint
+from textwrap import dedent
 
-BaseClass = Scene()
-BaseClass.enter()
-death_scene = Death()
-death_scene.enter()
-central_corridor = CentralCorridor()
-central_corridor.enter()
-laser_weapon_armory = LaserWeaponArmory()
-laser_weapon_armory.enter()
-the_bridge = TheBridge()
-escape_pod = EscapePod()
 
-laser_weapon_armory.enter()
+class Scene():
+    
+    def enter(self):
+        print("This scene is not yet configured.")
+        print("Subclass it and implement enter().")
+        exit(1)
 
-mapname = Map("death")
-print(mapname.start_scene)
-print(mapname.map_name['the_bridge'].enter)
-mapname.opening_scene() """
 
-#以上为测试
+
+class Engine():
+
+    def __init__(self, scene_map):
+        self.scene_map = scene_map
+
+    def play(self):
+        current_scene = self.scene_map
+        print("Engine.play()")
+
+
+class Death(Scene):
+
+    def enter(self):
+        pass
+
+
+class CentralCorridor(Scene):
+    
+    def enter(self):
+        pass
+
+
+class LaserWeaponArmory(Scene):
+
+    def enter(self):
+        pass
+
+
+class TheBridge(Scene):
+
+    def enter(self):
+        pass
+
+
+
+class EscapePod(Scene):
+
+    def enter(self):
+        pass
+
+
+class LastScene(Scene):
+
+    def enter(self):
+        pass
+
+
+class Map(object):
+    """docstring for Map."""
+
+    map_name ={"death":Death, "central_corridor":CentralCorridor,
+				"laser_weapon_armory":LaserWeaponArmory,
+				"the_bridge":TheBridge,
+				"escape_pod":EscapePod,
+				"lastscene":LastScene}
+
+    def __init__(self, start_scene):
+        self.start_scene = start_scene
+
+    def next_scene(self,scene_name):
+        pass
+    def opening_scene(self):
+        pass
+
+       
+
--- Learn PYTHON3 the HARD WAY/ex43_classes.py
+++ Learn PYTHON3 the HARD WAY/ex43_classes.py
@@ -144,7 +144,20 @@ class Map(object):
 
 
 
+BaseClass = Scene()
+BaseClass.enter()
+death_scene = Death()
+death_scene.enter()
+central_corridor = CentralCorridor()
+central_corridor.enter()
+laser_weapon_armory = LaserWeaponArmory()
+laser_weapon_armory.enter()
+the_bridge = TheBridge()
+escape_pod = EscapePod()
 
-		
+laser_weapon_armory.enter()
 
-		
+mapname = Map("death")
+print(mapname.start_scene)
+print(mapname.map_name['the_bridge'].enter)
+# mapname.opening_scene()
