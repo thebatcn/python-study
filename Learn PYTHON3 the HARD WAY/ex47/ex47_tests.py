2237ad232c07c7162fecbe0efbcbf598d25c8c7d
thebatcn <thebatcn1@hotmail.com>
2023-02-11 22:48:08 +0800

    ex47项目

---
+++ Learn PYTHON3 the HARD WAY/ex47/ex47_tests.py
@@ -0,0 +1,6 @@
+from game import Room
+
+room = Room("gold", """This room has gold in it you can grab. There's a
+            door to the north.""")
+
+print(room.name)
--- Learn PYTHON3 the HARD WAY/ex47/game.py
+++ Learn PYTHON3 the HARD WAY/ex47/game.py
@@ -5,4 +5,11 @@ class Room():
         self. description = description
         self.paths = {}
 
+    def go(self, direction):
+        return self.paths.get(direction, None)
+
+    def add_paths(self, paths):
+        self.paths.update(paths)
+
+
         
