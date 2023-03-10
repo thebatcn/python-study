6d095bea79970d4d798f9b71f8432145a61405b0
thebatcn <thebatcn1@hotmail.com>
2023-03-11 00:33:32 +0800

    lexicon

--- Learn_PYTHON3_the_HARD_WAY/ex48/lexicon.py
+++ Learn_PYTHON3_the_HARD_WAY/ex48/lexicon.py
@@ -3,37 +3,44 @@ directions = ['north', 'west', 'south', 'east',
 verb = ['go', 'stop', 'kill', 'eat']
 modifiers = ['the', 'in', 'of', 'from', 'at', 'it']
 noun = ['door', 'bear', 'princess', 'cabinet']
-nums = [num for num in range(0,101)] # 0-100的数字
+nums = [num for num in range(0, 101)]  # 0-100的数字
 vocabulary = [directions, verb, modifiers, noun, nums]
 wordtuple = []
 words = []
-stuff = input('> ').split()
-#转换其中的数字字符类型为int
-for a in stuff:  
-    try:
-        words.append(int(a))
-    except ValueError:
-        words.append(a)
-# print('stuff: ', stuff)
-# print('words: ', words)
-for word in words:  #多if 条件判断，应该加continue.
-    if word in directions:
-        wordtuple.append(('direction', word))
-        continue
-    if word in verb:
-        wordtuple.append(('verb', word))
-        continue
-    if word in modifiers:
-        wordtuple.append(('modifiers', word))
-        continue
-    if word in noun:
-        wordtuple.append(('noun', word))
-        continue
-    if word in nums:
-        wordtuple.append(('nums', word))
-        continue
-    else:
-        wordtuple.append(('errors', word))
-        continue
+# stuff = input('> ').split()
+# 转换其中的数字字符类型为int
 
-print(wordtuple)
+
+def lexicon_scan(stuff):
+    stuff = stuff.strip().split()
+    for a in stuff:
+        try:
+            words.append(int(a))
+        except ValueError:
+            words.append(a)
+    # print('stuff: ', stuff)
+    # print('words: ', words)
+    for word in words:  # 多if 条件判断，应该加continue.
+        if word in directions:
+            wordtuple.append(('direction', word))
+            continue
+        if word in verb:
+            wordtuple.append(('verb', word))
+            continue
+        if word in modifiers:
+            wordtuple.append(('modifiers', word))
+            continue
+        if word in noun:
+            wordtuple.append(('noun', word))
+            continue
+        if word in nums:
+            wordtuple.append(('nums', word))
+            continue
+        else:
+            wordtuple.append(('errors', word))
+            continue
+    return wordtuple
+
+
+# a = lexicon_scan(' I GO 8 it')
+# print(a)
