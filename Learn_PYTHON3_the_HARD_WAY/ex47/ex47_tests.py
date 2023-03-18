c7117d5fb59ad6b594837a44ffce6f684d1359a4
thebatcn <thebatcn1@hotmail.com>
2023-03-18 21:05:23 +0800

    用unittest模块修改

--- Learn_PYTHON3_the_HARD_WAY/ex47/ex47_tests.py
+++ Learn_PYTHON3_the_HARD_WAY/ex47/ex47_tests.py
@@ -1,33 +1,39 @@
-from nose.tools import *
-from ex47.game import Room
+import unittest
+from game import Room
 
 
-def test_room():
-    
-    gold = Room("gold", """This room has gold in it you can grab. There's a
-                door to the north.""")
+class TestGameRoom(unittest.TestCase):
 
-    assert_equal(gold.name, "GoldRoom")
-    assert_equal(gold.paths, {})
+    def test_room(self):
 
-def test_room_paths():
-    center = Room("Center", "Test room in the center.")
-    north = Room("North", "Test room in the north.")
-    south = Room("South", "Test room in the south.")
+        gold = Room("gold", """This room has gold in it you can grab. There's a
+                    door to the north.""")
 
-    center.add_paths({'north': north, 'south': south})
-    assert_equal(center.go('north'), north)
-    assert_equal(center.go("south"), south)
+        self.assertEqual(gold.name, "GoldRoom")
+        self.assertEqual(gold.paths, {})
 
-def test_map():
-    start = Room("start", "You can go west and down a hole.")
-    west = Room("Trees", "There are trees here, you can go east.")
-    down = Room("Dungeon", "It's dark down here, you can go up.")
+    def test_room_paths(self):
+        center = Room("Center", "Test room in the center.")
+        north = Room("North", "Test room in the north.")
+        south = Room("South", "Test room in the south.")
 
-    start.add_paths(({'west': west, 'down': down}))
-    west.add_paths({"east": start})
-    down.add_paths({"up": start})
+        center.add_paths({'north': north, 'south': south})
+        self.assertEqual(center.go('north'), north)
+        self.assertEqual(center.go("south"), south)
 
-    assert_equal(start.go('west'), west)
-    assert_equal(start.go("west").go("east"), start)
-    assert_equal(start.go("down").go('down').go('up'), start)
+    def test_map(self):
+        start = Room("start", "You can go west and down a hole.")
+        west = Room("Trees", "There are trees here, you can go east.")
+        down = Room("Dungeon", "It's dark down here, you can go up.")
+
+        start.add_paths(({'west': west, 'down': down}))
+        west.add_paths({"east": start})
+        down.add_paths({"up": start})
+
+        self.assertEqual(start.go('west'), west)
+        self.assertEqual(start.go("west").go("east"), start)
+        self.assertEqual(start.go("down").go('down').go('up'), start)
+
+
+if __name__ == "__main__":
+    unittest.main()
