b50438823ce41e016df9abfbfbce7d78b7b284c3
thebatcn <thebatcn1@hotmail.com>
2023-03-30 00:22:23 +0800

    添加

--- Learn_PYTHON3_the_HARD_WAY/projects/skeleton/gothonweb/app_test.py
+++ Learn_PYTHON3_the_HARD_WAY/projects/skeleton/gothonweb/app_test.py
@@ -1,5 +1,8 @@
 from unittest import *
 from app import app
 
-app.config['TESTING']=True
-web =app.test_client()
+app.config['TESTING'] = True
+web = app.test_client()
+def test_index():
+    rv = web.get('/', follow_redirects = True)
+    
