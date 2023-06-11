25f18ab4fd62ed49fe6ff0789d94eb66d25d1fde
thebatcn <thebatcn1@hotmail.com>
2023-06-11 19:41:28 +0800

    练习

---
+++ Learn_PYTHON3_the_HARD_WAY/projects/练手小项目/判断回文.py
@@ -0,0 +1,19 @@
+#偶数不是
+#奇数。判断第一个和最后一个，第二个和倒数第二个。。。依次判断，都相等是回文。
+
+def ispalindrome(s):
+    if len(s)%2 == 0:
+        return False
+    for i in range(len(s)//2):
+        if s[i] != s[-i-1]:
+            return False
+    return True
+    
+
+def main():
+    string = input("输入一个字符串： ")
+    print(ispalindrome(string))
+
+if __name__ == "__main__":
+    main()
+
