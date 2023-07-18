e83e9bc9ddc4acf294b82e5de703d9b61d811fa0
thebatcn <thebatcn1@hotmail.com>
2023-07-18 19:33:52 +0800

    练习

--- Learn_PYTHON3_the_HARD_WAY/projects/snake.py
+++ Learn_PYTHON3_the_HARD_WAY/projects/snake.py
@@ -8,11 +8,13 @@ SCREEN_SIZE = (640, 480)
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE, 0, depth=32)
 pygame.display.set_caption('One test')
+font = pygame.font.SysFont("arialblac",40)
+myfont = font.render("Game Over",True,(255,0,0))
 clock = pygame.time.Clock()
 SNAKE = [(100, 100), (100, 110), (100, 120)]
 FOOD = (randint(0, 639), randint(0, 479))
 SNAKE_SIZE = len(SNAKE)
-directions = [0, 10]
+directions = [10, 0]
 directions_old = [0, 0]
 snake_head = SNAKE[0]
 ACTION = True
@@ -22,13 +24,13 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_DOWN and ACTION:
+            if event.key == pygame.K_DOWN and ACTION and directions !=[0,-10]:
                 directions = [0, 10]
-            elif event.key == pygame.K_RIGHT and ACTION:
+            elif event.key == pygame.K_RIGHT and ACTION and directions !=[-10,0]:
                 directions = [10, 0]
-            elif event.key == pygame.K_UP and ACTION:
+            elif event.key == pygame.K_UP and ACTION and directions !=[0,10]:
                 directions = [0, -10]
-            elif event.key == pygame.K_LEFT and ACTION:
+            elif event.key == pygame.K_LEFT and ACTION and directions !=[10,0]:
                 directions = [-10, 0]
             elif event.key == pygame.K_SPACE and ACTION:
                 directions_old = directions
@@ -39,21 +41,36 @@ while True:
                 ACTION = not ACTION
 
     screen.fill((0, 0, 0))
+
     for dot in SNAKE:
-        pygame.draw.circle(screen, (255, 0, 255), dot, 5)
+        if dot == SNAKE[0]:
+            pygame.draw.circle(screen,(255,165,0),dot,5)
+        else:
+            pygame.draw.circle(screen, (255, 0, 255), dot, 5)
 
     pygame.draw.circle(screen, (0, 255, 0), FOOD, 5)
     clock.tick(10)
-    pygame.display.update()
 
     snake_head = (snake_head[0]+directions[0], snake_head[1]+directions[1])
     SNAKE_SIZE = len(SNAKE)
 
     if ACTION:
         if math.sqrt((snake_head[0]-FOOD[0])**2+(snake_head[1]-FOOD[1])**2) <= 10:
-            FOOD = (randint(0, 639), randint(0, 479))
+            FOOD = (randint(5, 634), randint(5, 474))
         else:
             SNAKE.pop()
         SNAKE.insert(0, snake_head)
 
-    print(ACTION)
+    if snake_head in SNAKE[1::]:
+        screen.blit(myfont,((screen.get_width()-myfont.get_width())/2,(screen.get_height()-myfont.get_height())/2))
+        ACTION = False
+        # break
+    if snake_head[0] <= 0 or snake_head[0] >= 639:
+        screen.blit(myfont,((screen.get_width()-myfont.get_width())/2,(screen.get_height()-myfont.get_height())/2))
+        ACTION = False
+        # break
+    if snake_head[1] <= 0 or snake_head[1] >= 479:
+        screen.blit(myfont,((screen.get_width()-myfont.get_width())/2,(screen.get_height()-myfont.get_height())/2))
+        ACTION = False
+        # break
+    pygame.display.flip()
---
+++ Learn_PYTHON3_the_HARD_WAY/projects/用python和pygame写游戏-从入门到精通/pygame(动画显示2种方法).py
@@ -0,0 +1,42 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+
+pygame.init()
+
+screen = pygame.display.set_mode((640, 480), 0, 32)
+
+#background = pygame.image.load(background_image_filename).convert()
+sprite = pygame.Surface((50,50),0,depth=32)
+sprite.fill((255,0,0))
+
+clock = pygame.time.Clock()
+
+# sprite的起始x坐标
+x = 0.
+
+myrect_x = 100
+while True:
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            exit()
+    
+    clock.tick(60)
+
+ #   screen.blit(background, (0,0))
+    screen.fill((0,0,0))
+    screen.blit(sprite, (x, 100))
+    x+= 4.     #如果你的机器性能太好以至于看不清，可以把这个数字改小一些
+    myrect = pygame.draw.rect(screen,(255,0,255),(myrect_x,100,50,50))
+    myrect_x +=2
+
+
+    # 如果移动出屏幕了，就搬到开始位置继续
+    if x > 640.:
+        x = 0.  
+    if myrect_x > 640:
+        myrect_x = 0  
+
+
+    pygame.display.update()
