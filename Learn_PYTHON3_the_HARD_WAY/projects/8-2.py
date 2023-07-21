63c4e7f06ddb9388f7325dadd32f569256120fa3
thebatcn <thebatcn1@hotmail.com>
2023-07-21 23:57:49 +0800

    添加等比例缩放

--- Learn_PYTHON3_the_HARD_WAY/projects/8-2.py
+++ Learn_PYTHON3_the_HARD_WAY/projects/8-2.py
@@ -11,6 +11,8 @@ pygame.display.set_caption('One test')
 
 sprite = pygame.image.load(sprite_image_filename)
 ball = pygame.image.load(ball_filename)
+scaled_image = pygame.transform.smoothscale(ball,(ball.get_size()[0]/3,ball.get_size()[1]/3)) #等比例缩放图片
+
 
 clock = pygame.time.Clock()
 
@@ -23,7 +25,7 @@ while True:
             pygame.quit()
             sys.exit()
     screen.fill((0,0,0))        
-    screen.blit(ball,(x,y))
+    screen.blit(scaled_image,(x,y))
 
     time_passed = clock.tick(1200)
     time_passed_second = time_passed / 1000.0
@@ -31,16 +33,16 @@ while True:
     x += time_passed_second * speed_x
     y += time_passed_second * speed_y
 
-    if x > 640 - ball.get_width():
-        x = 640 - ball.get_width()
+    if x > 640 - scaled_image.get_width():
+        x = 640 - scaled_image.get_width()
         speed_x = -speed_x
     elif x < 0:
         speed_x = -speed_x
         x = 0
 
-    if y > 480 - ball.get_height():
+    if y > 480 - scaled_image.get_height():
         speed_y = -speed_y
-        y = 480 - ball.get_height()
+        y = 480 - scaled_image.get_height()
     elif y < 0 :
         speed_y = -speed_y
         y = 0
