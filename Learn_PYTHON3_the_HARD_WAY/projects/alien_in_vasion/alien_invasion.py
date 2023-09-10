aec50f55da3beb000d754b730c0c3c8864d2d2b9
thebatcn <thebatcn1@hotmail.com>
2023-09-10 20:36:50 +0800

    adding

--- Learn_PYTHON3_the_HARD_WAY/projects/alien_in_vasion/alien_invasion.py
+++ Learn_PYTHON3_the_HARD_WAY/projects/alien_in_vasion/alien_invasion.py
@@ -1,17 +1,23 @@
 import pygame
 import sys
+from settings import Settings
+
+ai_settings = Settings()
 
 def run_game():
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
+    # screen = pygame.display.set_mode((1200,800))
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Vasion")
 
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                pygame.quit()
                 sys.exit()
-
+        # screen.fill((230,230,230))
+        screen.fill((ai_settings.bg_color))
         pygame.display.flip()
     
 run_game()
