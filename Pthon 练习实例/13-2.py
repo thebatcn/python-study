276fce02fe9ff5d9363d42d26447f118999968ce
thebatcn <thebatcn1@hotmail.com>
2023-11-05 01:11:29 +0800

    Group版本，显示星星数量多余实际的

--- Pthon 练习实例/13-2.py
+++ Pthon 练习实例/13-2.py
@@ -3,15 +3,25 @@ from random import randint
 import pygame
 import sys
 from pygame.sprite import Group
+from time import sleep
+
 SCREEN_SIZE = (640,480)
 
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE,0,depth=32)
 star_image = pygame.image.load('star.bmp')
-nums_star = randint(1,20)
-print(nums_star)
-stars = []
 
+
+class Star(pygame.sprite.Sprite):
+    """定义一个Star的类"""
+    def __init__(self,image, x, y):
+        super().__init__()
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        
+star_group = Group()
 pygame.display.set_caption('One test')
 while True:
     for event in pygame.event.get():
@@ -22,15 +32,23 @@ while True:
             if event.key==pygame.K_q:
                 pygame.quit()
                 sys.exit()
+                
+    nums_star = randint(1,5)
+    # print(nums_star)
+    star_group.empty()
 
+    for i in range(nums_star):
+        star_sprite = Star(star_image, randint(0,640), randint(0,480))
+        star_group.add(star_sprite)
     screen.fill((0,0,0))
-    
     # for i in range(nums_star):
     #     stars.append((randint(0,640),randint(0,480)))
-    stars = [(randint(0,640),randint(0,480)) for i in range(nums_star)]
-    print("nums_star: ",len(stars))
-    for star in stars:
-        screen.blit(star_image,star)
-    
-       
-    pygame.display.update()
+    # stars = [(randint(0,640),randint(0,480)) for i in range(nums_star)]
+    # print("nums_star: ",len(stars))
+    # for star in stars:
+    #     screen.blit(star_image,star)
+    # print(len(star_group))
+    star_group.draw(screen)
+    pygame.display.flip()
+    # sleep(2)
+# 显示的星星数量多余nums_star
