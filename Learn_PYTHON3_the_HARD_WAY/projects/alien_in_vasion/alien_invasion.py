50798c878831188ce29a24b3a9d10b6fd43912a3
thebatcn <thebatcn1@hotmail.com>
2023-12-10 13:24:26 +0800

    1

--- Learn_PYTHON3_the_HARD_WAY/projects/alien_in_vasion/alien_invasion.py
+++ Learn_PYTHON3_the_HARD_WAY/projects/alien_in_vasion/alien_invasion.py
@@ -29,7 +29,7 @@ def run_game():
     stats = GameStats(ai_settings)
 
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats,play_button,ship, bullets)
         if stats.game_active:
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
--- Learn_PYTHON3_the_HARD_WAY/projects/alien_in_vasion/game_functions.py
+++ Learn_PYTHON3_the_HARD_WAY/projects/alien_in_vasion/game_functions.py
@@ -63,7 +63,7 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ai_settings, screen,stats,play_button, ship, bullets):
     """响应按键和鼠标事件"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -73,6 +73,15 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x,mouse_y = pygame.mouse.get_pos()
+            check_play_button(stats,play_button,mouse_x,mouse_y)
+            
+def check_play_button(stats,play_button,mouse_x,mouse_y):
+    """在玩家单击Play按钮时开始新游戏"""
+    if play_button.rect.collidepoint(mouse_x,mouse_y):
+        stats.game_active = True
+        
 
 
 def update_screen(ai_settings, screen,stats, ship, aliens, bullets,play_button):
@@ -89,7 +98,7 @@ def update_screen(ai_settings, screen,stats, ship, aliens, bullets,play_button):
         play_button.draw_button()
     # 让最近绘制的屏幕可见
     pygame.display.flip()
-
+    
 
 def update_bullets(ai_settings,screen,ship,aliens,bullets):
     """更新子弹的位置，并删除已消失的子弹"""
