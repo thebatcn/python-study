7a8bdb20f636d0d1b46396425004d5cf491d9f2f
thebatcn <thebatcn@google.com>
2024-06-19 14:24:39 +0800

    1

---
+++ fun_arg(几种不同函数参数).py
@@ -0,0 +1,27 @@
+def function(p1, /, p_k, arg_k1, *, arg_k2):
+    """def f(pos1, pos2, /, pos_or_kwd, *, kwd1, kwd2):
+  -----------    ----------     ----------
+    |             |                  |
+    |        Positional or keyword   |
+    |                                - Keyword only
+     -- Positional only
+/ 和 * 是可选的。这些符号表明形参如何把参数值传递给函数：位置、位置或关键字、关键字。关键字形参也叫作命名形参。
+"""
+    print(p1)
+    # for v in p_k:
+    # 	print(v,end = ' ')
+    print(p_k)
+    print(arg_k1)
+    print(arg_k2)
+
+
+function(134, 'a', arg_k1="abcdefg", arg_k2='[[[[]]]]')
+
+
+def foo(p1, *p2, p_k):
+    print(p1)
+    print(p2)
+    print(p_k)
+
+
+foo(234, 'a', 'b', p_k='234ab')
--- tkinter/tktest.py
+++ tkinter/tktest.py
@@ -2,32 +2,48 @@ from tkinter import *
 from tkinter import ttk
 import re
 
+## 
+# root = Tk()
+# root.title("Hello World!")
 
+# def check_num(newval):
+#     return re.match('^[0-9]*$', newval) is not None and len(newval) <= 5
+# check_num_wrapper = (root.register(check_num), '%P')
+
+# username = StringVar()
+# frm = ttk.Frame(root, padding=10)
+# frm.grid()
+# ttk.Label(frm, text="Hello World.", background="red").grid(row=10, column=0)
+# ttk.Button(frm, text="Quit", command=root.destroy).grid(row=10, column=1)
+# Lb2 = ttk.Label(frm, text="go in", font="Arial", foreground="RED").grid(
+#     column=1, row=11)
+# label2 = ttk.Label(text=username) 
+# label2.grid()
+# name = ttk.Entry(frm,textvariable = username,width = 8)
+# name.grid()
+# name.bind('<Return>',lambda e: label2.configure(text = username.get()))
+# num = StringVar()
+# e = ttk.Entry(root, textvariable=num, validate='key', validatecommand=check_num_wrapper)
+# e.grid(column=0, row=0, sticky='we')
+
+# root.mainloop()
+
+#Listbox练习
 root = Tk()
-root.title("Hello World!")
+frame = ttk.Frame(root,width=400,height=180,relief='ridge',padding=(12,12,12,12))
+frame.grid(row=0,column=0,padx=5)
 
-def check_num(newval):
-    return re.match('^[0-9]*$', newval) is not None and len(newval) <= 5
-check_num_wrapper = (root.register(check_num), '%P')
+choices = ["apple","pear","orange"]
+choicesvar = StringVar(value=choices)
+l = Listbox(frame,listvariable=choicesvar,height=10)
 
-username = StringVar()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World.", background="red").grid(row=10, column=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(row=10, column=1)
-Lb2 = ttk.Label(frm, text="go in", font="Arial", foreground="RED").grid(
-    column=1, row=11)
-label2 = ttk.Label(text=username) 
-label2.grid()
-name = ttk.Entry(frm,textvariable = username,width = 8)
-name.grid()
-name.bind('<Return>',lambda e: label2.configure(text = username.get()))
-num = StringVar()
-e = ttk.Entry(root, textvariable=num, validate='key', validatecommand=check_num_wrapper)
-e.grid(column=0, row=0, sticky='we')
+l.grid(row=1,column=0,)
+
+choices.append("peach")
+choices.append("banana")
+choicesvar.set(choices)
 
 root.mainloop()
-
     
 
 
