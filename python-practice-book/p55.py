a14f90484d8e99b04f28e468b5884f4910dfd211
thebatcn <thebatcn@google.com>
2024-06-29 14:00:37 +0800

    2

--- python-practice-book/p55.py
+++ python-practice-book/p55.py
@@ -28,14 +28,19 @@ def anagrams(word_list):
     result_have=[]
     new_list =[''.join(sorted(w)) for w in words_list]
     # print(new_list)
-
+    """ 版本1
+    # for w in words_list:
+    #     sort_w ="".join(sorted(w))
+    #     word_dict.setdefault(sort_w, []).append(w)
+    #     if  sort_w in new_list and new_list.count(sort_w) > 1:
+    #         result_have.append(w)
+    #     else:
+    #         result_no.append(w)
+    # return result_no, result_have"""
     for w in words_list:
         sort_w ="".join(sorted(w))
-        if  sort_w in new_list and new_list.count(sort_w) > 1:
-            result_have.append(w)
-        else:
-            result_no.append(w)
-    return result_no, result_have
+        word_dict.setdefault(sort_w, []).append(w)
+    return word_dict.values()
 #目前实现分辨单词是否anagrams，并且输出的功能。
 #改进版，实现用字典储存结果，根据单词长度分别储存
 #最终版，字典储存，能根据构成字母类型一致的，储存在一组。长度可以作为排行依据。
