719bdf5865e9c1737626731d7da0334fc649433b
thebatcn <thebatcn@google.com>
2024-06-29 11:29:49 +0800

    练习题

--- python-practice-book/p55.py
+++ python-practice-book/p55.py
@@ -1,12 +1,23 @@
-def anagrams(lst):
-	"""To find anagrams in a given list of words. Two words are called anagrams if one word can be formed by rearranging letters of another. For example ‘eat’, ‘ate’ and ‘tea’ are anagrams."""
-	
-	word_dict = {}
-	lengths = set(len(word) for word in lst)
-	word_dict={length:[word for word in lst if  len(word)==length] for length in lengths}
+#未编写完成
+def anagrams(words):
+    """To find anagrams in a given list of words. Two words are called anagrams if one word can be formed by rearranging letters of another. For example 'eat, 'ate' and 'tea' are anagrams."""
+    result = []
+    for i in range(len(words)):
+        for j in range(i + 1, len(words)):
+            if sorted(words[i]) == sorted(words[j]):
+                if not result:
+                    result.append([words[i], words[j]])
+                else:
+                    found = False
+                    for group in result:
+                        if words[i] in group:
+                            group.append(words[j])
+                            found = True
+                            break
+                    if not found:
+                        result.append([words[i], words[j]])
+    return result
 
-	print(word_dict)
-	
-
-words = ['eat', 'ate', 'done', 'tea', 'soup', 'node']
-anagrams(words)
+# 示例：
+words_list = ['eat', 'ate', 'done', 'tea', 'soup', 'node']
+print(anagrams(words_list))
